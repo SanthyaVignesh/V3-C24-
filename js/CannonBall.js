5ffff83029328bbc9821c1efd5290d2d10272ca7
SanthyaVignesh <74100644+SanthyaVignesh@users.noreply.github.com>
2021-07-11 15:53:06 +0530

    Add files via upload

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -8,7 +8,7 @@ class CannonBall{
         }
         this.r = 40;
         this.body = Bodies.circle(x,y,this.r,options);
-        this.image = loadImage("../assets/cannonball.png");
+        this.image = loadImage("assets/cannonball.png");
 
         World.add(world,this.body);
     }
